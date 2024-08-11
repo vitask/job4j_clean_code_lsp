64bdd19026f42f5725590991580e33adeec485b7
vitask <skalenko.v@yandex.ru>
2024-08-11 10:38:55 +0700

    1. Создать меню. [#4748 #508557]

--- src/main/java/ru/job4j/ood/isp/menu/Menu.java
+++ src/main/java/ru/job4j/ood/isp/menu/Menu.java
@@ -23,8 +23,12 @@ public interface Menu extends Iterable<Menu.MenuItemInfo> {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()){
+                return false;
+            }
             MenuItemInfo that = (MenuItemInfo) o;
             return Objects.equals(name, that.name)
                     && Objects.equals(children, that.children)
